033c554536d93bc0b099342a3989fb601e5f80b4
nikhil-soni27 <nikhilsoni1030@gmail.com>
2022-01-24 13:22:22 +0530

    chatbot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React,{useState} from "react";
+import {IoIosSend} from 'react-icons/io'
 
 function App() {
+
+  const [message, setmessage] = useState([]);
+  const [messagename, setmessagename] = useState("");
+
+  const minimize = () => {
+    let chatbot = document.querySelector(".chatbot").style;
+    if (chatbot.height !== "7rem") {
+      chatbot.height = "7rem";
+    } else {
+      chatbot.height = "70vh";
+    }
+  }
+  const submitHandler = (event) => {
+    event.preventDefault();
+  }  
+  const addMessage=()=>{
+    setmessage([
+      ...message,
+      {
+        name: messagename
+      }
+    ])
+    setmessagename("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="container">
+      <div className="chatbot">
+      <div className="nav">
+        <img src="https://ai.axisbank.co.in/morfeuswebsdk/libs/websdk/images/minimize.svg" onClick={minimize}></img>
+</div>
+        <div className="right-sender">
+            {message.map(msg => (
+              <div className="msg">
+              <div className="sender-side">
+              <p>{msg.name}</p>
+              </div>
+              <div className="chat-circle"></div>
+              </div>
+              ))}
+              </div>
+        
+      <form onSubmit={submitHandler}>
+        <label>
+          <input
+            name="item"
+            type="text"
+            className="text"
+            placeholder="Type Here"
+            value={messagename}
+            onChange={e => setmessagename(e.target.value)}
+            />
+            <IoIosSend className="send" onClick={addMessage}/>
+          </label>
+           </form>
+            </div>
+            </div>
+    </>
   );
 }
 
+
 export default App;
+
